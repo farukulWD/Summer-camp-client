125613054947a4ae11df31f311b62fc4e8df986a
Farukul Islam <farukgb1999@gmail.com>
2023-06-06 23:19:01 +0600

    routes setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes";
+import Main from "./Layout/Main";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}>
+      <Main></Main>
+    </RouterProvider>
+  </React.StrictMode>
+);
